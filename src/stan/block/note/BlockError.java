38c88cd67f7d57e96bd59a26e12da806bb097e87
kepocnhh <kepocnhh@gmail.com>
2014-11-25 06:42:30 +0400

    Переставляю проекты

--- src/stan/block/note/BlockError.java
+++ src/stan/block/note/BlockError.java
@@ -3,8 +3,12 @@ package stan.block.note;
 public class BlockError
 	extends BlockBase
 {
+//Поля
     String message;
-    
+
+//Методы
+
+//Конструкторы
     public BlockError(String m)
     {
         this.message = m;
